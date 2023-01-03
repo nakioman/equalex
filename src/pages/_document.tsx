666617550d5e7fd05fa09e715c87fa9bb54edd3e
Ignacio Glinsek <iglinsek@secureauth.com>
2023-01-03 15:45:24 -0300

    Fixed CSS generation
    https://github.com/ant-design/ant-design/issues/39891

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,52 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta name="description" content="Investment performance portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const cache = createCache();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          (
+            <StyleProvider cache={cache}>
+              <App {...props} />
+            </StyleProvider>
+          ),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {/* This is hack, `extractStyle` does not currently support returning JSX or related data. */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `</script>${extractStyle(cache)}<script>`,
+            }}
+          />
+        </>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="description" content="Investment performance portfolio" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
