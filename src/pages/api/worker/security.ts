a046b7b6496a913c1280152225fb9a3934d83ce2
Ignacio Glinsek <iglinsek@secureauth.com>
2023-01-04 16:06:54 -0300

    Added button to update a ticker manually

--- src/pages/api/worker/security.ts
+++ src/pages/api/worker/security.ts
@@ -1,10 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getToken } from 'next-auth/jwt';
 import cleanSecurities from '../../../modules/worker/api/cleanSecurities';
 import updateSecurities from '../../../modules/worker/api/updateSecurities';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const desiredToken = process.env.WORKER_API_KEY;
-  if (req.headers['api-key'] !== desiredToken) return res.status(401).end();
+  const token = await getToken({ req });
+  if (!token && req.headers['api-key'] !== desiredToken) return res.status(401).end();
 
   switch (req.method) {
     case 'DELETE':
